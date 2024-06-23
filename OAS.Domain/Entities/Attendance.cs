867e192a70156c816b96d928c4a6f5333d67e387
GilbertoHernandez150 <ghernandezds@gmail.com>
2024-06-23 00:00:15 -0300

    Add files via upload

---
+++ OAS.Domain/Entities/Attendance.cs
@@ -0,0 +1,16 @@
+﻿using OAS.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAS.Domain.Entities
+{
+    public class Attendance : BaseEntity
+    {
+        public int EmployeeId { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime? CheckOutTime { get; set; }
+    }
+}
---
+++ OAS.Domain/Entities/Employee.cs
@@ -0,0 +1,16 @@
+﻿using OAS.Domain.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAS.Domain.Entities
+{
+    public class Employee : Person
+    {
+        public string EmployeeId { get; set; }
+        public DateTime HireDate { get; set; }
+        public int DepartmentId { get; set; }
+    }
+}
